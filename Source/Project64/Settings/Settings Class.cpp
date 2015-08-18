41a58ce1a7659a86d278c78c4509a0435502ce5e
AmbientMalice <AmbientMalice@gmail.com>
2015-08-18 22:05:58 +1000

    Make Azimer WIP default audio plugin.

--- Source/Project64/Settings/Settings Class.cpp
+++ Source/Project64/Settings/Settings Class.cpp
@@ -312,7 +312,7 @@ void CSettings::AddHowToHandleSetting ()
 	//Plugin
 	AddHandler(Plugin_RSP_Current,   new CSettingTypeApplication("Plugin","RSP Dll",       "RSP\\RSP 1.7.dll"));
 	AddHandler(Plugin_GFX_Current,   new CSettingTypeApplication("Plugin","Graphics Dll",  "GFX\\PJ64Glide64.dll"));
-	AddHandler(Plugin_AUDIO_Current, new CSettingTypeApplication("Plugin","Audio Dll",     "Audio\\Jabo_Dsound.dll"));
+	AddHandler(Plugin_AUDIO_Current, new CSettingTypeApplication("Plugin","Audio Dll",     "Audio\\AziAudio.dll"));
 	AddHandler(Plugin_CONT_Current,  new CSettingTypeApplication("Plugin","Controller Dll","Input\\PJ64_NRage.dll"));
 
 	AddHandler(Plugin_RSP_CurVer,    new CSettingTypeApplication("Plugin","RSP Dll Ver",        ""));
